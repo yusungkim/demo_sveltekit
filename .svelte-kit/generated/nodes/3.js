766c5d5b17041a41201dad43a95694cb762f728c
YusungKim <yusungkim@me.com>
2022-12-10 22:46:45 +0900

    delete build artifact

--- .svelte-kit/generated/nodes/3.js
+++
@@ -1,2 +0,0 @@
-export { default as component } from "../../../src/routes/movies/+page.svelte";
-export const server = true;
--- .svelte-kit/generated/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as shared from "../../../src/routes/shop/+page.ts";
-export { shared };
-export { default as component } from "../../../src/routes/shop/+page.svelte";
