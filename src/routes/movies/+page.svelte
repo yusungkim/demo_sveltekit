60d04e805c76cad5cf01491c6082b65d18bb6e64
YusungKim <yusungkim@me.com>
2022-12-18 09:24:50 +0900

    dockerize: fix node_volumes

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
   export let data;
-  console.log("Data Received and Rendered Page: /shop/+page.svelte")
+  console.log("Data Received and Rendered Page: /shop/+page.svelte");
 
   let { results: movies } = data;
+  console.log(movies);
 </script>
 
 <h1 class="text-4xl py-3">Popular Movies</h1>
 
-<ul>
+<div class="grid grid-cols-1 xl:grid-cols-2">
   {#each movies as movie}
-    <li class="p-2">
-      <h2 class="text-3xl">{movie.title}</h2>
-      <p>{movie.overview}</p>
-    </li>
+    <div class="p-2">
+      <div class="flex gap-2">
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          alt="img"
+          class="w-28 object-cover"
+        />
+        <div>
+          <div class="flex gap-2 justify-between items-start">
+            <h2 class="text-3xl">{movie.title}</h2>
+            <div class="flex flex-col items-end min-w-fit">
+              <div>{movie.release_date}</div>
+              <div>({movie.vote_average})</div>
+            </div>
+          </div>
+          <p class="">{movie.overview}</p>
+        </div>
+      </div>
+    </div>
   {/each}
-</ul>
+</div>
